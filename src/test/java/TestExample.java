7273d09a00ef4349e8a0d73669e55eaf7724b931
Lautaro Damore <ldamore@proyecto404.com>
2020-03-31 12:54:29 -0300

    set environment

---
+++ src/test/java/TestExample.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestExample {
+    @Test
+    public void testJunit(){
+
+        assertEquals(0, 0);
+    }
+}
